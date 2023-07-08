7f474353db2dee6fb2b9614942356d48b13109f2
yan-maksi <102655652+yan-maksi@users.noreply.github.com>
2023-07-08 23:24:36 +0300

    Update parser_site.py

--- parser_site.py
+++ parser_site.py
@@ -74,92 +74,96 @@ def scrape(url):
     driver.get(url)
 
     full_data_about_coin = []
-    # Simulate scrolling to load dynamic content
-    for i in range(16):  # The number of times of scrolls 14
-        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        sleep(2)  # Add a delay to allow content to load (adjust as needed)
+    try:
+        # Simulate scrolling to load dynamic content
+        for i in range(14):  # The number of times of scrolls 16
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            sleep(2)  # Add a delay to allow content to load (adjust as needed)
 
-        # Retrieve the updated HTML content
-        html_content = driver.page_source
+            # Retrieve the updated HTML content
+            html_content = driver.page_source
 
-        # Create a BeautifulSoup object using the updated HTML content
-        coin_html_content = BeautifulSoup(html_content, 'html.parser')
-        containers = coin_html_content.findAll("div", {"class": "white-desk ico-card"})
-        print(f'len of conteiner {len(containers)} in {i} iteration')
+            # Create a BeautifulSoup object using the updated HTML content
+            coin_html_content = BeautifulSoup(html_content, 'html.parser')
+            containers = coin_html_content.findAll("div", {"class": "white-desk ico-card"})
+            print(f'len of conteiner {len(containers)} in {i} iteration')
 
-        # take the data we need from all ico
-        for coin in containers:
-            try:
-                interest = coin.find("div", class_="nr").text
-            except AttributeError:
-                interest = coin.find("div", class_="all_site_val").text
+            # take the data we need from all ico
+            for coin in containers:
+                try:
+                    interest = coin.find("div", class_="nr").text
+                except AttributeError:
+                    interest = coin.find("div", class_="all_site_val").text
 
 
-            category = coin.find("div", class_="categ_type").text
-            try:
-                received_money = coin.find("div", id="new_column_categ_invisted").text.replace('\n', '')
-            except AttributeError:
-                received_money = coin.find("span", id="notset").text
+                category = coin.find("div", class_="categ_type").text
+                try:
+                    received_money = coin.find("div", id="new_column_categ_invisted").text.replace('\n', '')
+                except AttributeError:
+                    received_money = coin.find("span", id="notset").text
 
-            try:
-                goal = coin.find("span", class_="notset").text
-            except AttributeError:
-                goal = coin.find("div", id="categ_desctop").find("span").text
+                try:
+                    goal = coin.find("span", class_="notset").text
+                except AttributeError:
+                    goal = coin.find("div", id="categ_desctop").find("span").text
 
-            end_date = coin.find("div", class_="date").text.replace('\n', '')
+                end_date = coin.find("div", class_="date").text.replace('\n', '')
 
-            coin_ticker = coin.find("div", class_="ico-main-info").find("h3").text.replace("\n", "")
+                coin_ticker = coin.find("div", class_="ico-main-info").find("h3").text.replace("\n", "")
 
-            link_to_coin_page = coin.find("div", class_="ico-main-info").contents[1].contents[0].attrs['href']
-            # print(f'Link to coin page: {link_to_coin_page}')
+                link_to_coin_page = coin.find("div", class_="ico-main-info").contents[1].contents[0].attrs['href']
+                # print(f'Link to coin page: {link_to_coin_page}')
 
-            # parse coin page
-            soup_content, response = scrape_page(link_to_coin_page)
-            if response.status_code != 403:
-                soup = soup_content.findAll("div", {"class": "col-12 col-lg-10"})[0]
+                # parse coin page
+                soup_content, response = scrape_page(link_to_coin_page)
+                if response.status_code != 403:
+                    soup = soup_content.findAll("div", {"class": "col-12 col-lg-10"})[0]
 
-                sold_coins = soup.find('div', {'class': 'goal'}).contents
-                if len(sold_coins) < 2:
-                    sold_coins = None
-                else:
-                    sold_coins = sold_coins[2]
+                    sold_coins = soup.find('div', {'class': 'goal'}).contents
+                    if len(sold_coins) < 2:
+                        sold_coins = None
+                    else:
+                        sold_coins = sold_coins[2]
 
-                # get start and end date coin sell
-                start_end_date_coin_sell = soup.find('div', {'class': 'col-12 title-h4'}).contents[3].text.replace('\n', '').replace(
-                    'Token Sale:', '').replace('Token Sale:', '')
+                    # get start and end date coin sell
+                    start_end_date_coin_sell = soup.find('div', {'class': 'col-12 title-h4'}).contents[3].text.replace('\n', '').replace(
+                        'Token Sale:', '').replace('Token Sale:', '')
 
-                # get all data about current coin
-                row_list_data = soup.find("div", class_="row list").findAll('span')
+                    # get all data about current coin
+                    row_list_data = soup.find("div", class_="row list").findAll('span')
 
-                coin_data_dict: dict = {}
-                for data in row_list_data:
+                    coin_data_dict: dict = {}
+                    for data in row_list_data:
+                        try:
+                            if data.next == 'Ticker: ':
+                                continue
+                            else:
+                                coin_data_dict[data.next.replace(':', "")] = data.nextSibling
+
+                        except Exception as e:
+                            print(f'Error when try to parse coin data: {e}')
+                            pass
+
                     try:
-                        if data.next == 'Ticker: ':
-                            continue
-                        else:
-                            coin_data_dict[data.next.replace(':', "")] = data.nextSibling
-
+                        role_of_token = soup.find("div", class_="col-12 info-analysis-list").findAll('span')[0].nextSibling
                     except Exception as e:
                         print(f'Error when try to parse coin data: {e}')
                         pass
 
-                try:
-                    role_of_token = soup.find("div", class_="col-12 info-analysis-list").findAll('span')[0].nextSibling
-                except Exception as e:
-                    print(f'Error when try to parse coin data: {e}')
-                    pass
+                    main_page_data = {"interest": interest, "category": category, "received_money": received_money,
+                                       "goal": goal, "end_date": end_date, "coin_ticker": coin_ticker,
+                                       "sold_coins": sold_coins,
+                                       "start_end_date_coin_sell": start_end_date_coin_sell,
+                                       "role_of_token": role_of_token}
 
-                main_page_data = {"interest": interest, "category": category, "received_money": received_money,
-                                   "goal": goal, "end_date": end_date, "coin_ticker": coin_ticker,
-                                   "sold_coins": sold_coins,
-                                   "start_end_date_coin_sell": start_end_date_coin_sell,
-                                   "role_of_token": role_of_token}
+                    # add data from data coin page to main page
+                    main_page_data.update(coin_data_dict)
+                    # print(main_page_data)
+                    full_data_about_coin.append(main_page_data)
 
-                # add data from data coin page to main page
-                main_page_data.update(coin_data_dict)
-                # print(main_page_data)
-                full_data_about_coin.append(main_page_data)
-
+    except Exception as e:
+        print(f'Error: {e}')
+        pass
 
     # Extract unique keys from the JSON data
     print(f'Len of full_data_about_coin afret: {len(full_data_about_coin)}')
@@ -167,17 +171,16 @@ def scrape(url):
     for obj in full_data_about_coin:
         if key_includes_number(obj):
             delete_column_with_number = delete_key_with_number(obj)
-            print(f'json object with number: {obj}')
             try:
                 all_keys.update(delete_column_with_number)
             except Exception as e:
                 pass
-            print("The JSON key includes a number.")
         else:
             all_keys.update(obj.keys())
 
+    print(all_keys)
     # Create an empty DataFrame with the extracted keys as column names
-    df = pd.DataFrame(columns=list(all_keys))
+    df = pd.DataFrame(columns=sorted(all_keys))
 
     # Iterate over the JSON objects and populate the DataFrame
     for obj in full_data_about_coin:
@@ -185,7 +188,7 @@ def scrape(url):
         df = pd.concat([df, pd.DataFrame([row])], ignore_index=True)
 
     # Write the DataFrame to a CSV file
-    df.to_csv('data.csv', index=False)
+    df.to_csv('ico.csv', index=False)
 
 
 if __name__ == "__main__":
